e03db7580323be6fd6d51609158efb0c18b74e97
Dylan Gilbert <39006574@parisnanterre.fr>
2024-05-25 14:29:08 +0200

    ApartmentBanner image unique

--- src/components/apartmentbanner/ApartmentBanner.jsx
+++ src/components/apartmentbanner/ApartmentBanner.jsx
@@ -5,8 +5,7 @@ function ApartmentBanner({ pictures }) {
   const [currentPicture, setCurrentPicture] = useState(0);
 
   const getClassName = (i) => {
-    if (i === currentPicture) return "show";
-    return "";
+    return i === currentPicture ? "show" : "";
   };
 
   const moveToNext = () => {
@@ -14,13 +13,20 @@ function ApartmentBanner({ pictures }) {
   };
 
   const moveToPrevious = () => {
-    const newCurrentPicture = currentPicture - 1;
-    if (newCurrentPicture < 0) {
-      setCurrentPicture(pictures.length - 1);
-      return;
-    }
-    setCurrentPicture(currentPicture - 1);
+    setCurrentPicture(
+      (currentPicture - 1 + pictures.length) % pictures.length
+    );
   };
+
+  if (pictures.length === 1) {
+    return (
+      <div className="apartment_banner">
+        <div className="image_container">
+          <img src={pictures[0]} alt="" className="show"></img>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="apartment_banner">
@@ -29,16 +35,21 @@ function ApartmentBanner({ pictures }) {
           <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
         ))}
       </div>
-      <button className="btn btn-previous" onClick={moveToPrevious}>
-        <i className="fas fa-chevron-left"></i>
-      </button>
-      <span className="slide-counter">
-        {currentPicture + 1} / {pictures.length}
-      </span>
-      <button className="btn btn-next" onClick={moveToNext}>
-        <i className="fas fa-chevron-right"></i>
-      </button>
+      {pictures.length > 1 && (
+        <>
+          <button className="btn btn-previous" onClick={moveToPrevious}>
+            <i className="fas fa-chevron-left"></i>
+          </button>
+          <span className="slide-counter">
+            {currentPicture + 1} / {pictures.length}
+          </span>
+          <button className="btn btn-next" onClick={moveToNext}>
+            <i className="fas fa-chevron-right"></i>
+          </button>
+        </>
+      )}
     </div>
   );
 }
+
 export default ApartmentBanner;
